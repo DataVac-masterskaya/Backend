946ae07a8693aa073fbd009f8cc4f7f5a5992e6c
Marat Garifulun <admin@admin.com>
2026-06-09 21:29:06 +0700

    urls_edited

--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import InfectionViewSet
+from .views import InfectionViewSet, IngredientsViewSet
 
 router = routers.DefaultRouter()
 router.register('infections', InfectionViewSet, basename='infections')
+router.register('ingredients', IngredientsViewSet, basename='ingredients')
 
 urlpatterns = [
     path('', include(router.urls)),
