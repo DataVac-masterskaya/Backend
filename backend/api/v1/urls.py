911737156dfebd6e60d3fdab26a0b6e15f1b3319
Константин Бородич <117466606+KoBorodich@users.noreply.github.com>
2026-06-09 23:13:13 +0400

    Подключить инструкции через общий API v1

--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import InfectionViewSet, IngredientsViewSet
+from .views import InfectionViewSet, IngredientsViewSet, SearchSelectView
 
 router = routers.DefaultRouter()
 router.register('infections', InfectionViewSet, basename='infections')
@@ -10,4 +10,6 @@ router.register('ingredients', IngredientsViewSet, basename='ingredients')
 urlpatterns = [
     path('', include(router.urls)),
     path('', include('contraindications.urls')),
+    path('', include('instructions.urls')),
+    path('search/select', SearchSelectView.as_view(), name='search-select'),
 ]
