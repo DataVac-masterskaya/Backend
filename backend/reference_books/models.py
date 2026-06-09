1a80837867331fe62a020ffea2ffdef675b6ea55
Marat Garifulun <admin@admin.com>
2026-06-09 23:33:58 +0700

    correct_tests

--- backend/reference_books/models.py
+++ backend/reference_books/models.py
@@ -2,9 +2,10 @@ from django.db import models
 from django.utils.text import Truncator
 
 from datavac.constants import (
-    LENGTH_TYPE_INGREDIENTS, NUMBER_WORDS_NAME, NUMBER_WORDS_TYPE,)
-
-#  from vaccine_cards.models import VaccineCardVersion
+    LENGTH_TYPE_INGREDIENTS,
+    NUMBER_WORDS_NAME,
+    NUMBER_WORDS_TYPE,
+)
 
 
 class SearchStatsMixin(models.Model):
