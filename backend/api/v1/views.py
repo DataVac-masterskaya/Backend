a975c8844a8174926b1bc7dcf71014c4b1df0d59
Marat Garifulun <admin@admin.com>
2026-06-09 23:33:58 +0700

    correct_tests

--- backend/api/v1/views.py
+++ backend/api/v1/views.py
@@ -4,9 +4,13 @@ from rest_framework import filters, viewsets
 from rest_framework.response import Response
 
 from reference_books.models import Infection, Ingredients
+
 from .filters import InfectionFilter, OrderingFilterSortBy
 from .serializers import (
-    InfectionCartSerializer, InfectionSerializer, IngredientsSerializer,)
+    InfectionCartSerializer,
+    InfectionSerializer,
+    IngredientsSerializer,
+)
 
 
 class InfectionViewSet(viewsets.ReadOnlyModelViewSet):
