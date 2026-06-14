9b0e67acc34c354e743b4446ef9bf2b3d6585852
Marat Garifulun <admin@admin.com>
2026-06-14 20:25:22 +0700

    correct_tests

--- backend/reference_books/tests/tests_ingredients.py
+++ backend/reference_books/tests/tests_ingredients.py
@@ -1,5 +1,6 @@
-from django.test import TestCase
 from django.core.exceptions import ValidationError
+from django.test import TestCase
+
 from reference_books.models import Ingredients
 
 
@@ -14,13 +15,13 @@ class IngredientsModelTest(TestCase):
         )
 
     def test_valid_ingredient_creation(self):
-        """Проверка корректного создания объекта"""
+        """Проверка корректного создания объекта."""
         self.assertEqual(self.ingredient.name, 'Гидроксид алюминия')
         self.assertEqual(self.ingredient.type, 'adjuvant')
         self.assertEqual(self.ingredient.description, 'Описание ингредиента')
 
     def test_required_fields(self):
-        """Проверка обязательных полей"""
+        """Проверка обязательных полей."""
         with self.assertRaises(ValidationError):
             Ingredients.objects.create(type='adjuvant')  # отсутствует name
 
@@ -28,7 +29,7 @@ class IngredientsModelTest(TestCase):
             Ingredients.objects.create(name='Тестовое')  # отсутствует type
 
     def test_name_max_length(self):
-        """Проверка максимальной длины поля name"""
+        """Проверка максимальной длины поля name."""
         max_length = Ingredients._meta.get_field('name').max_length
         long_name = 'a' * (max_length + 1)
 
@@ -39,7 +40,7 @@ class IngredientsModelTest(TestCase):
             )
 
     def test_type_validation(self):
-        """Проверка валидности типа"""
+        """Проверка валидности типа."""
         valid_types = ['adjuvant', 'stabilizer', 'active']
 
         for type in valid_types:
@@ -51,7 +52,7 @@ class IngredientsModelTest(TestCase):
             self.assertEqual(ingredient.type, type)
 
     def test_unique_name(self):
-        """Проверка уникальности названия"""
+        """Проверка уникальности названия."""
         with self.assertRaises(ValidationError):
             Ingredients.objects.create(
                 name=self.ingredient.name,
@@ -59,11 +60,11 @@ class IngredientsModelTest(TestCase):
             )
 
     def test_str_method(self):
-        """Проверка метода __str__"""
+        """Проверка метода __str__."""
         self.assertEqual(str(self.ingredient), self.ingredient.name)
 
     def test_description_optional(self):
-        """Проверка необязательности поля description"""
+        """Проверка необязательности поля description."""
         ingredient = Ingredients.objects.create(
             name='Тестовое',
             type='adjuvant'
