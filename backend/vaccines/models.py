7cf647d4e3c085e6a732e71b4d8b1724c3ad7186
MeTi <tagaevmukhammad@gmail.com>
2026-06-21 16:24:12 +0300

    Update vaccintes.models

--- backend/vaccines/models.py
+++ backend/vaccines/models.py
@@ -1,7 +1,19 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
-from vaccines.constants import CHAR_MAX_LEN
+from vaccines.constants import (
+    DECIMAL_PLACES,
+    IS_AVAILABLE_IN_RF_MAX_LEN,
+    MAX_AGE_MAX_LEN,
+    MAX_DIGITS_SEARCH_WEIGHT,
+    MIN_AGE_MAX_LEN,
+    NAME_MAX_LEN,
+    OFFICIAL_NAME_MAX_LEN,
+    PREGNANCY_USAGE_STATUS,
+    STATUS_MAX_LEN,
+    URL_MAX_LEN,
+    VERSION_STATUS_MAX_LEN,
+)
 
 User = get_user_model()
 
@@ -10,19 +22,46 @@ class VaccineCard(models.Model):
     """Карточка вакцины."""
 
     current_version = models.ForeignKey(
-        'VaccineCardVersion', null=True, on_delete=models.SET_NULL, related_name='cards_as_current'
+        'VaccineCardVersion',
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='cards_as_current',
+        verbose_name='Текущая версия в работе',
     )
     published_version = models.ForeignKey(
-        'VaccineCardVersion', null=True, on_delete=models.SET_NULL, related_name='cards_as_published'
+        'VaccineCardVersion',
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='cards_as_published',
+        verbose_name='Опубликованная версия',
     )
-    status = models.CharField(max_length=CHAR_MAX_LEN, blank=True, null=True)
-    is_visible = models.BooleanField(default=False)
-    search_select_count = models.PositiveIntegerField(default=0)
-    search_weight = models.PositiveIntegerField(default=0)
-    created_by = models.ForeignKey(User, on_delete=models.PROTECT, related_name='created_vaccine_cards')
-    updated_by = models.ForeignKey(User, on_delete=models.PROTECT, related_name='updated_vaccine_cards')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=STATUS_MAX_LEN, blank=True, null=True, verbose_name='Общий статус карточки')
+    is_visible = models.BooleanField(default=False, verbose_name='Видимость в поиске')
+    search_select_count = models.PositiveBigIntegerField(default=0, verbose_name='Счетчик запросов')
+    search_weight = models.DecimalField(
+        default=0.00,
+        max_digits=MAX_DIGITS_SEARCH_WEIGHT,
+        decimal_places=DECIMAL_PLACES,
+        verbose_name='Популярность (вес)',
+    )
+    created_by = models.ForeignKey(
+        User, null=True, on_delete=models.PROTECT, related_name='created_vaccine_cards', verbose_name='Автор карточки'
+    )
+    updated_by = models.ForeignKey(
+        User,
+        null=True,
+        on_delete=models.PROTECT,
+        related_name='updated_vaccine_cards',
+        verbose_name='Редактор карточки',
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата создания',
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Дата редактирования',
+    )
 
     class Meta:
         verbose_name = 'Карточка вакцины'
@@ -37,30 +76,134 @@ class VaccineCard(models.Model):
 class VaccineCardVersion(models.Model):
     """Версия карточки вакцины."""
 
-    vaccine_card = models.ForeignKey(VaccineCard, related_name='versions', on_delete=models.CASCADE)
-    version_number = models.PositiveIntegerField(blank=True, null=True)
-    version_status = models.CharField(max_length=CHAR_MAX_LEN, blank=True, null=True)
-    parent_version = models.ForeignKey('self', null=True, on_delete=models.SET_NULL, related_name='children')
-    moderation_request = models.PositiveIntegerField(blank=True, null=True)
-    name = models.CharField(max_length=CHAR_MAX_LEN, blank=True, null=True)
-    official_name = models.CharField(max_length=CHAR_MAX_LEN, blank=True, null=True)
-    is_available_in_rf = models.BooleanField(default=False)
-    revision_date = models.DateField(blank=True, null=True)
-    min_age = models.PositiveIntegerField(blank=True, null=True)
-    max_age = models.PositiveIntegerField(blank=True, null=True)
-    pregnancy_usage_status = models.CharField(max_length=CHAR_MAX_LEN, blank=True, null=True)
-    storage_conditions = models.TextField(blank=True, null=True)
-    interaction_info = models.TextField(blank=True, null=True)
-    compatibility_info = models.TextField(blank=True, null=True)
-    comment_ANO = models.TextField(blank=True, null=True)
-    ohlp_url = models.URLField(blank=True, null=True)
-    nonspec_url = models.URLField(blank=True, null=True)
-    instruction_url = models.URLField(blank=True, null=True)
-    pdf_url = models.URLField(blank=True, null=True)
-    created_by = models.ForeignKey(User, on_delete=models.PROTECT, related_name='created_versions')
-    approved_by = models.ForeignKey(User, on_delete=models.PROTECT, related_name='approved_versions', null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    approved_at = models.DateTimeField(blank=True, null=True)
+    vaccine_card = models.ForeignKey(
+        VaccineCard, related_name='versions', on_delete=models.CASCADE, verbose_name='Ссылка на контейнер карточки'
+    )
+    version_number = models.IntegerField(
+        blank=True,
+        null=True,
+        verbose_name='Порядковый номер версии',
+    )
+    version_status = models.CharField(
+        max_length=VERSION_STATUS_MAX_LEN,
+        blank=True,
+        null=True,
+        verbose_name='Статус версии',
+    )
+    parent_version = models.ForeignKey(
+        'self',
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='children',
+        verbose_name='Предыдущая версия',
+    )
+    moderation_request = models.PositiveIntegerField(
+        blank=True,
+        null=True,
+        verbose_name='Ссылка на запрос модерации',
+    )  # TODO пока как заглушка, потом ссылка на таблицу модерации
+    name = models.CharField(
+        max_length=NAME_MAX_LEN,
+        blank=True,
+        null=True,
+        verbose_name='Название вакцины',
+    )
+    official_name = models.CharField(
+        max_length=OFFICIAL_NAME_MAX_LEN,
+        blank=True,
+        null=True,
+        verbose_name='Официальное название',
+    )
+    is_available_in_rf = models.CharField(
+        max_length=IS_AVAILABLE_IN_RF_MAX_LEN,
+        blank=True,
+        null=True,
+        verbose_name='Доступность в РФ',
+    )
+    revision_date = models.DateField(
+        blank=True,
+        null=True,
+        verbose_name='Дата ревизии',
+    )
+    min_age = models.CharField(max_length=MIN_AGE_MAX_LEN, blank=True, null=True, verbose_name='Минимальный возраст')
+    max_age = models.CharField(
+        max_length=MAX_AGE_MAX_LEN,
+        blank=True,
+        null=True,
+        verbose_name='Максимальный возраст',
+    )
+    pregnancy_usage_status = models.CharField(
+        max_length=PREGNANCY_USAGE_STATUS,
+        blank=True,
+        null=True,
+        verbose_name='Применение при беременности',
+    )
+    storage_conditions = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name='Условия хранения',
+    )
+    interaction_info = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name='Информация о взаимодействии с препаратами',
+    )
+    compatibility_info = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name='Информация о совместимости с другими вакцинами',
+    )
+    comment_ANO = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name='Комментарий АНО',
+    )
+    ohlp_url = models.URLField(
+        max_length=URL_MAX_LEN,
+        blank=True,
+        null=True,
+        verbose_name='Ссылка на сайт ОХЛП',
+    )
+    nonspec_url = models.URLField(
+        max_length=URL_MAX_LEN,
+        blank=True,
+        null=True,
+        verbose_name='Ссылка на вкладыш',
+    )
+    pdf_url = models.URLField(
+        max_length=URL_MAX_LEN,
+        blank=True,
+        null=True,
+        verbose_name='Ссылка на PDF',
+    )
+    instruction_url = models.URLField(
+        max_length=URL_MAX_LEN,
+        blank=True,
+        null=True,
+        verbose_name='Ссылка на инструкцию',
+    )
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        related_name='created_versions',
+        verbose_name='Кем создана версия',
+    )
+    approved_by = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        related_name='approved_versions',
+        null=True,
+        verbose_name='Кем проверена версия',
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Когда создана версия',
+    )
+    approved_at = models.DateTimeField(
+        blank=True,
+        null=True,
+        verbose_name='Кем проверена версия',
+    )
 
     class Meta:
         verbose_name = 'Версия вакцины'
