9effe5c328a628d019ad54acbc1b8fa36a3a9b1c
Gasuaska <gasuaska2@gmail.com>
2026-06-25 01:49:52 +0500

    Поправлены ошибки в линтере

--- backend/users/tests/test_models.py
+++ backend/users/tests/test_models.py
@@ -1,5 +1,4 @@
 import pytest
-
 from django.contrib.auth import get_user_model
 
 from users.models import Profile
@@ -8,19 +7,19 @@ User = get_user_model()
 
 @pytest.mark.django_db
 def test_profile_created_when_user_created():
-    '''Проверяет, что Profile создается при создании пользователя'''
+    """Проверяет, что Profile создается при создании пользователя."""
     user = User.objects.create_user(username='test', password='12345')
     assert Profile.objects.filter(user=user).exists()
 
 @pytest.mark.django_db
 def test_one_profile_per_user():
-    '''Проверяет, что создается только один Profile для пользователя'''
+    """Проверяет, что создается только один Profile для пользователя."""
     user = User.objects.create_user(username='test', password='12345')
     assert Profile.objects.filter(user=user).count() == 1
 
 @pytest.mark.django_db
 def test_profile_user_relation():
-    '''Проверяет, что Profile создается для конкретного пользователя'''
+    """Проверяет, что Profile создается для конкретного пользователя."""
     user = User.objects.create_user(username='test', password='12345')
     profile = Profile.objects.get(user=user)
     assert profile.user == user
