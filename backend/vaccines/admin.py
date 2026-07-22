510c39f7f0b407ac3444c125daa68c13b7d7ac4f
MeTi <tagaevmukhammad@gmail.com>
2026-07-22 17:22:17 +0300

    add save_model methot for vaccines

--- backend/vaccines/admin.py
+++ backend/vaccines/admin.py
@@ -169,6 +169,14 @@ class VaccineCardVersionAdmin(admin.ModelAdmin):
         ),
     )
 
+    def save_model(self, request, obj, form, change):
+        if not change:
+            if obj.vaccine_card and not obj.vaccine_card.current_version:
+                obj.vaccine_card.current_version = obj
+                obj.vaccine_card.published_version = obj
+                obj.vaccine_card.save()
+        super().save_model(request, obj, form, change)
+
 
 @admin.register(VaccineCard)
 class VaccineCardAdmin(admin.ModelAdmin):
