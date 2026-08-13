a0b8ae9f63bbec877c1dbfb76a23ca24e2d8612a
Gasuaska <gasuaska2@gmail.com>
2026-08-14 02:34:32 +0500

    тестово заполнен contraindication_type

--- backend/contraindications/management/commands/set_test_db.py
+++ backend/contraindications/management/commands/set_test_db.py
@@ -4,7 +4,7 @@ from django.core.management.base import BaseCommand
 
 from contraindications.models import Contraindication, ContraindicationCategory
 from reference_books.models import Infection, Ingredients
-from vaccines.models import VaccineCard, VaccineCardVersion
+from vaccines.models import VaccineCard, VaccineCardVersion, VaccineCardVersionAdministrationMethod
 
 
 class Command(BaseCommand):
@@ -38,6 +38,14 @@ class Command(BaseCommand):
             ingredient.search_select_count = randint(1, 100)
             ingredient.popularity = randint(1, 100)
             ingredient.save(update_fields=['search_select_count', 'popularity'])
+
+        for i, vaccine_method in enumerate(
+            VaccineCardVersionAdministrationMethod.objects.all()):
+            if vaccine_method.pk % 2 == 0:
+                vaccine_method.contraindication_type = 'Временное'
+            else:
+                vaccine_method.contraindication_type = 'Абсолютное'
+            vaccine_method.save(update_fields=['contraindication_type'])
 
         for i, infection in enumerate(Infection.objects.all()):
             infection.search_select_count = randint(1, 100)
